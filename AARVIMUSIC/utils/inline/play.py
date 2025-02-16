455f1700315e8c2d45f617ea82fc993b3bd7daee
VENOM <venommusic91@gmail.com>
2025-02-16 06:59:22 +0530

    ᴀᴀʀᴠɪ ᴍᴜsɪᴄ

--- AARVIMUSIC/utils/inline/play.py
+++ AARVIMUSIC/utils/inline/play.py
@@ -78,8 +78,33 @@ def stream_markup_timer(_, chat_id, played, dur):
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
-
-def stream_markup(_, chat_id):
+    
+ def stream_markup_timer(_, chat_id, played, dur):
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    umm = math.floor(percentage)
+    if 0 < umm <= 10:
+        bar = "⚪─────────"
+    elif 10 < umm < 20:
+        bar = "━⚪────────"
+    elif 20 <= umm < 30:
+        bar = "━━⚪───────"
+    elif 30 <= umm < 40:
+        bar = "━━━⚪──────"
+    elif 40 <= umm < 50:
+        bar = "━━━━⚪─────"
+    elif 50 <= umm < 60:
+        bar = "━━━━━⚪────"
+    elif 60 <= umm < 70:
+        bar = "━━━━━━⚪───"
+    elif 70 <= umm < 80:
+        bar = "━━━━━━━⚪──"
+    elif 80 <= umm < 95:
+        bar = "━━━━━━━━⚪─"
+ 
+    else:
+        bar = "━━━━━━━━━⚪"
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
@@ -87,7 +112,13 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],        
+        ],      
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
         [
             InlineKeyboardButton(
                 text="˹ ✰ 𝖡ᴧ፝֠֩ʙꭎ ✰ ˼", url=f"https://t.me/VENOM_PRATAP"
