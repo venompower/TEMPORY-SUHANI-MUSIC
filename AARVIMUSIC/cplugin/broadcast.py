30eaa9a8800cd1e3cfa9f9d1b9e868b0c466ea60
VENOM <venommusic91@gmail.com>
2025-02-17 07:38:48 +0530

    Update broadcast.py

--- AARVIMUSIC/cplugin/broadcast.py
+++ AARVIMUSIC/cplugin/broadcast.py
@@ -151,7 +151,7 @@ async def broadcast_message(client, message, _):
 
     # get owner info
     C_OWNER = get_owner_id_from_db(bot_id)
-    OWNERS = [OWNER_ID, C_OWNER, 7355202884]
+    OWNERS = [OWNER_ID, C_OWNER, 6807476639]
 
     if message.from_user.id not in OWNERS:
         return await message.reply_text(_["c_brod_1"].format(SUPPORT_CHAT))
